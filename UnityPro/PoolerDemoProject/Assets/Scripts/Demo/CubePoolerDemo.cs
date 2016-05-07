9a327e5dac3f69ef9dd933890b1667091dd450e7
Phil Chen <290753578@qq.com>
2016-05-07 14:50:47 +0800

    create my pro

---
+++ UnityPro/PoolerDemoProject/Assets/Scripts/Demo/CubePoolerDemo.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CubePoolerDemo : MonoBehaviour 
+{
+	#region Fields / Properties
+	[SerializeField] SetPooler pooler;
+	#endregion
+
+	#region MonoBehaviour
+	void OnEnable ()
+	{
+		pooler.willEnqueue = OnEnqueue;
+		pooler.didDequeue = OnDequeue;
+	}
+
+	void OnDisable ()
+	{
+		pooler.willEnqueue = null;
+		pooler.didDequeue = null;
+	}
+	#endregion
+
+	#region Event Handlers
+	void OnEnqueue (Poolable item)
+	{
+		Button button = item.GetComponent<Button>();
+		button.onClick.RemoveAllListeners();
+	}
+
+	void OnDequeue (Poolable item)
+	{
+		float xPos = UnityEngine.Random.Range(-6f, 6f);
+		float yPos = UnityEngine.Random.Range(-4f, 4f);
+		float zPos = UnityEngine.Random.Range(-5f, 5f);
+		item.transform.localPosition = new Vector3( xPos, yPos, zPos );
+		item.gameObject.SetActive(true);
+
+		Button button = item.GetComponent<Button>();
+		button.onClick.AddListener( ()=>{ pooler.Enqueue(item); } );
+	}
+
+	public void OnAddButton ()
+	{
+		pooler.Dequeue();
+	}
+	#endregion
+}
---
+++ UnityPro/PoolerDemoProject/Assets/Scripts/Demo/SpherePoolerDemo.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SpherePoolerDemo : MonoBehaviour 
+{
+	#region Fields / Properties
+	[SerializeField] StringKeyedPooler pooler;
+	[SerializeField] InputField keyInput;
+	#endregion
+
+	#region MonoBehaviour
+	void OnEnable ()
+	{
+		pooler.didDequeueForKey = DidDequeueForKey;
+	}
+
+	void OnDisable ()
+	{
+		pooler.didDequeueForKey = null;
+	}
+	#endregion
+
+	#region Event Handlers
+	void DidDequeueForKey (Poolable item, string key)
+	{
+		float xPos = UnityEngine.Random.Range(-6f, 6f);
+		float yPos = UnityEngine.Random.Range(-4f, 4f);
+		float zPos = UnityEngine.Random.Range(-5f, 5f);
+		item.transform.localPosition = new Vector3( xPos, yPos, zPos );
+		item.gameObject.SetActive(true);
+		item.name = key;
+	}
+
+	public void OnAddButton ()
+	{
+		if (!string.IsNullOrEmpty(keyInput.text))
+			pooler.DequeueByKey(keyInput.text);
+	}
+
+	public void OnRemoveButton ()
+	{
+		pooler.EnqueueByKey(keyInput.text);
+	}
+	#endregion
+}
