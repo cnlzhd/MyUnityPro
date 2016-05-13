a631fdd328b21d5fbc7eecfd35a314598080e1e7
chenning02 <chenning02@gyyx.cn>
2016-05-13 14:34:26 +0800

    新建资源文件

---
+++ UnityPro/PoolerDemoProject/Assets/Res/CellPoolerDemo.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CellPoolerDemo : MonoBehaviour 
+{
+	#region Fields / Properties
+	[SerializeField] IndexedPooler pooler;
+	[SerializeField] RectTransform content;
+	#endregion
+
+	#region MonoBehaviour
+	void OnEnable ()
+	{
+		pooler.didDequeueAtIndex = DidDequeueAtIndex;
+		pooler.willEnqueueAtIndex = WillEnqueueAtIndex;
+	}
+
+	void OnDisable ()
+	{
+		pooler.didDequeueAtIndex = null;
+		pooler.willEnqueueAtIndex = null;
+	}
+	#endregion
+
+	#region Event Handlers
+	void DidDequeueAtIndex (Poolable item, int index)
+	{
+		Button button = item.GetComponent<Button>();
+		button.transform.SetParent(content, false);
+		button.gameObject.SetActive(true);
+		button.onClick.AddListener( () => {
+			Colorize(button.GetComponent<Image>());
+		} );
+	}
+
+	void WillEnqueueAtIndex (Poolable item, int index)
+	{
+		Button button = item.GetComponent<Button>();
+		button.onClick.RemoveAllListeners();
+	}
+
+	public void OnAddButton ()
+	{
+		pooler.Dequeue();
+	}
+
+	public void OnColorizeButton ()
+	{
+		for (int i = 0; i < pooler.Collection.Count; ++i)
+			Colorize( pooler.GetScript<Image>(i) );
+	}
+	#endregion
+
+	#region Private
+	void Colorize (Image image)
+	{
+		float r = Random.value;
+		float g = Random.value;
+		float b = Random.value;
+		image.color = new Color(r, g, b);
+	}
+	#endregion
+}
